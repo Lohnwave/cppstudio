203e8a1162f344c3c7a594b9578fc3f41f2f22fe
Lohnwave <waveluozu@163.com>
2021-04-11 17:30:15 +0800

    add xgboost todo

---
+++ comm/xgboost-1.4.0/dmlc-core/src/build_config.cc
@@ -0,0 +1,18 @@
+/*
+ * @Author: Lohnwave
+ * @Date: 2021-04-11 17:20:32
+ * @LastEditors: Lohnwave
+ * @LastEditTime: 2021-04-11 17:23:40
+ * @Descripttion: 
+ * @version: 
+ */
+
+#include <dmlc/base.h>
+
+#ifdef DMLC_EMIT_FOPEN64_REDEFINE_WARNING
+    #ifdef _MSC_VER
+        #pragma message(": warning : Redefining fopen64 with std::fopen")
+    #else
+        #warning "Redefining fopen64 with std::fopen"
+    #endif
+#endif
